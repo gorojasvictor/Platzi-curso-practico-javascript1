0e3152775e9b2ecbce9c12292a6afc5d5b113abc
gorojasvictor <victor.rojas1981@gmail.com>
2021-07-27 15:42:35 -0500

    encapsulando console.log en funciones

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Codigo del Cuadrado
-console.group("Cuadrado");
+/*console.group("Cuadrado");
 const  ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cms");
 
@@ -57,4 +57,35 @@ console.log("El perimetro del circulo es de: " + perimetroCirculo + "cm")
 // Area
 const areaCirculo = (radioCirculo * radioCirculo) * PI;
 console.log("El area del circulo es de: " + areaCirculo + "cm2")
-console.groupEnd();
+console.groupEnd();*/
+
+const PI = Math.PI;
+
+function perimetroCuadrado(lado){
+  return lado * 4;
+};
+
+function areaCuadrado(lado){
+  return lado * lado;
+};
+
+function perimetroTriangulo(lado1, lado2, base){
+  return lado1 + lado2 + base;
+};
+
+function areaTriangulo(base, altura){
+  return (base * altura) / 2;
+};
+
+function diametroCirculo(radio){
+  return radio * 2;
+};
+
+function perimetroCirculo(radio){
+  const diametro = diametroCirculo(radio);
+    return diametro * PI;
+};
+
+function areaCirculo(radio){
+  return (radio * radio) * PI;
+};

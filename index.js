605f94d466b1260fe6100df5a29fab49b98b8301
gorojasvictor <victor.rojas1981@gmail.com>
2021-07-27 11:50:45 -0500

    primeras formulas

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const  ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado)
